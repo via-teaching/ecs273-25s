0de11fc69895be3a68b36aa694fcf829522f2863
Yu-Chia Huang <ycihuang@ucdavis.edu>
2025-04-25 14:40:47 -0700

    header

--- react-example/src/App.tsx
+++ react-example/src/App.tsx
@@ -11,15 +11,17 @@ export default function App() {
   const bars: CategoricalBar[] = Data.data;
   return (
     <div className="flex flex-col h-full w-full">
-      <header className="bg-zinc-400 text-white p-2 flex flex-row">
+      <header className="bg-zinc-400 text-white p-2 flex flex-row align-center">
         <h2 className="text-left text-2xl">Homework 3</h2>
-        <select id = 'bar-select' className="bg-white text-black p-2 rounded mx-2">
-            {bars.map((bar, index) => (
-            <option key={index} value={bar.category}>
-              {bar.category}
-            </option>
-            ))}
-        </select>
+        <label htmlFor="bar-select" className="mx-2">Select a category:
+          <select id = 'bar-select' className="bg-white text-black p-2 rounded mx-2">
+              {bars.map((bar, index) => (
+              <option key={index} value={bar.category}>
+                {bar.category}
+              </option>
+              ))}
+          </select>
+        </label>
       </header>
       <div className="flex flex-row h-full w-full">
         <div className="flex flex-col w-2/3">
