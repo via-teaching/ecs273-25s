1c2a5fe5ff4672ffde0f3cd7d7e510f8c4544cd3
Yu-Chia Huang <ycihuang@ucdavis.edu>
2025-04-25 14:13:19 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Homework 3 Templates
+
+This repository contains multiple templates:
+
+- `react-example`: A template based on react and typescript.
+
+## Usage
+
+Each template is self-contained. Navigate into a folder and install dependencies:
+
+```bash
+cd react-example
+npm install
+npm run dev
+```
---
+++ react-example/src/App.tsx
@@ -0,0 +1,40 @@
+import { BarChart } from "./component/example";
+import Data from "../data/demo.json";
+import { Bar } from "./types";
+// A "extends" B means A inherits the properties and methods from B.
+interface CategoricalBar extends Bar{
+    category: string;
+}
+
+
+export default function App() {
+  const bars: CategoricalBar[] = Data.data;
+  return (
+    <div className="flex flex-col h-full w-full">
+      <header className="bg-zinc-400 text-white p-2 flex flex-row">
+        <h2 className="text-left text-2xl">Homework 3</h2>
+        <select id = 'bar-select' className="bg-white text-black p-2 rounded mx-2">
+            {bars.map((bar, index) => (
+            <option key={index} value={bar.category}>
+              {bar.category}
+            </option>
+            ))}
+        </select>
+      </header>
+      <div className="flex flex-row h-full w-full">
+        <div className="flex flex-col w-2/3">
+          <div className="border-2 border-gray-300 rounded-xl p-2 h-1/2 m-2 view">
+            {BarChart()}
+          </div>
+          <div className="border-2 border-gray-300 rounded-xl p-2 h-1/2 m-2 view">
+            <p className="text-center text-gray-500 mt-20">Empty View 2</p>
+          </div>
+        </div>
+        <div className="border-2 border-gray-300 rounded-xl p-2 w-1/3 m-2 view">
+          <p className="text-center text-gray-500 mt-20">Empty View 3</p>
+        </div>
+      </div>
+    </div>
+    
+  );
+}
