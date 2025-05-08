d0e6bd47e51819d6979bca6a320f737386efd687
manami-bunbun <manami2622@gmail.com>
2025-05-08 15:07:23 -0700

    change options

--- Homework3/react-js-template/src/component/options.jsx
+++ Homework3/react-js-template/src/component/options.jsx
@@ -1,10 +1,17 @@
 import Data from "../../data/demo.json";
 
 export default function RenderOptions() {
-    const bars = Data.data;
-    return bars.map((bar, index) => (
-      <option key={index} value={bar.category}>
-        {bar.category}
+
+    const tickers = [
+      "AAPL", "MSFT", "GOOGL", "AMZN", "META",
+      "NVDA", "TSLA", "JPM", "V", "UNH",
+      "HD", "PG", "DIS", "MA", "PEP",
+      "KO", "BAC", "CVX", "XOM", "INTC"
+    ];
+    // const bars = Data.data;
+    return tickers.map((ticker, index) => (
+      <option key={index} value={ticker}>
+        {ticker}
       </option>
     ));
   }
